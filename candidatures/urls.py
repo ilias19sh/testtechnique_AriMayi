342e7af9e683ee998a678f2441eaa93fd136dd4f
ilias19sh <hanfaouiilias4@gmail.com>
2025-03-21 14:25:49 +0100

    ajout du Swagger

--- candidatures/urls.py
+++ candidatures/urls.py
@@ -1,10 +1,27 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CandidateViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = DefaultRouter()
 router.register(r'candidates', CandidateViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Recruitment API",
+        default_version="v1",
+        description="API pour gérer les candidats",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
